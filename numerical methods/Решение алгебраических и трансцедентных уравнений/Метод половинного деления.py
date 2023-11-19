923f953e99315ca01f40bce96aa65a6842ffb9ac
Alex Chiling <aleksandraspenova@gmail.com>
2023-11-19 16:37:49 +0300

    задание 1

--- numerical methods/Решение алгебраических и трансцедентных уравнений/Метод половинного деления.py
+++ numerical methods/Решение алгебраических и трансцедентных уравнений/Метод половинного деления.py
@@ -1,20 +1,17 @@
 # x^3 + 4x - 6 = 0
+def first_func(e,a,b,n):
+    while True:
+        x = (a + b)/2
+        f1 = (a ** 3) + (4 * a) - 6 # <- f(a)
+        f2 = (x ** 3) + (4 * x) - 6 # <- f(x)
+        if f1 * f2 > 0:
+            a = x
+        else:
+            b = x
+        n += 1
+        if abs(b - a) < e  :
+            break
+    return(x, f2, n)
 
-e = 0.0001
-a = -5
-b = 5
-n = 0
-
-while True:
-    x = (a + b)/2
-    f1 = (a ** 3) + (4 * a) - 6 # <- f(a)
-    f2 = (x ** 3) + (4 * x) - 6 # <- f(x)
-    if f1 * f2 > 0:
-        a = x
-    else:
-        b = x
-    n += 1
-    print(x, f2, n)
-    if abs(b - a) < e  :
-        break
+print(first_func(0.0001,-5,5,0))
 
